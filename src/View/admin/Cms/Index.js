c40b97fccfa8c461509d74480c5324b78b947c60
jmmon <jmmon93@gmail.com>
2023-06-04 09:54:05 -0700

    added missing keys to map; removed unused imports; formatting

--- src/View/admin/Cms/Index.js
+++ src/View/admin/Cms/Index.js
@@ -1,17 +1,17 @@
 import {
-    Card,
-    Table,
-    Stack,
-    Avatar,
-    Button,
-    Checkbox,
-    TableRow,
-    TableBody,
-    TableCell,
-    Container,
-    Typography,
-    TableContainer,
-    TablePagination
+  Card,
+  //Table,
+  Stack,
+  //Avatar,
+  Button,
+  //Checkbox,
+  //TableRow,
+  //TableBody,
+  //TableCell,
+  Container,
+  Typography
+  //TableContainer,
+  //TablePagination
 } from '@mui/material';
 import plusFill from '@iconify/icons-eva/plus-fill';
 import trash from '@iconify/icons-eva/trash-2-fill';
@@ -19,97 +19,92 @@ import editfill from '@iconify/icons-eva/edit-fill';
 import Label from '../../../components/Label';
 
 import { Icon } from '@iconify/react';
-import { Link as RouterLink } from 'react-router-dom';
-import DataTable from "react-data-table-component";
-import DataTableExtensions from "react-data-table-component-extensions";
-import "react-data-table-component-extensions/dist/index.css";
-
+//import { Link as RouterLink } from 'react-router-dom';
+import DataTable from 'react-data-table-component';
+import DataTableExtensions from 'react-data-table-component-extensions';
+import 'react-data-table-component-extensions/dist/index.css';
 
 import Page from '../../../components/Page';
 
 export default function Index(props) {
+  const columns = [
+    { name: 'Name', selector: 'name', sortable: true },
 
-    const columns = [
+    {
+      name: 'Status',
+      cell: (row) => (
+        <>
+          <Label variant="ghost" color={(row.status === 1 && 'success') || 'error'}>
+            {row.status === 1 ? 'Active' : 'Inactive'}
+          </Label>
+        </>
+      ),
+      ignoreRowClick: true,
+      allowOverflow: true
+    },
 
-        { name: "Name", selector: "name", sortable: true },
+    {
+      name: 'Actions',
+      cell: (row) => (
+        <>
+          <button className="btn btn-danger btn-sm" onClick={() => props.deleteCms(row._id)}>
+            <Icon icon={trash} />
+          </button>
+          &nbsp;
+          <button className="btn btn-sm btn-primary" onClick={() => props.editCms(row)}>
+            <Icon icon={editfill} />
+          </button>
+        </>
+      ),
+      ignoreRowClick: true,
+      allowOverflow: true,
+      button: true
+    }
+  ];
+  const data = [];
+  if (props.cmsList && props.cmsList.length > 0) {
+    props.cmsList.map((cms) => {
+      data.push(cms);
+    });
+  }
 
-        {
-            name: "Status",
-            cell: (row) => <>
-           
-                <Label
-                    variant="ghost"
-                    color={(row.status === 1 && 'success') || 'error'}
-                >
-                    {row.status === 1 ? "Active" : "Inactive"}
-                </Label>
-            </>,
-            ignoreRowClick: true,
-            allowOverflow: true,
-        },
+  const tableData = {
+    columns,
+    data,
+    export: false,
+    print: false
+  };
+  return (
+    <Page title="Cms Page | CMS">
+      {/* {console.log(data)} */}
 
-        {
-            name: "Actions",
-            cell: (row) => <>
-                <button className="btn btn-danger btn-sm" onClick={(e) => props.deleteCms(row._id)}><Icon icon={trash} /></button>&nbsp;
-                <button className="btn btn-sm btn-primary" onClick={() => props.editCms(row)}><Icon icon={editfill} /></button>
-            </>,
-            ignoreRowClick: true,
-            allowOverflow: true,
-            button: true,
-        },
-
-
-    ];
-    const data = [];
-    if (props.cmsList && props.cmsList.length > 0) {
-        props.cmsList.map((cms) => {
-            data.push(cms);
-        },
-        );
-    };
-
-
-    const tableData = {
-        columns,
-        data,
-        export: false,
-        print: false,
-    };
-    return (
-
-        <Page title="Cms Page | CMS">
-            {/* {console.log(data)} */}
-
-            <Container>
-                <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
-                    <Typography variant="h4" gutterBottom>
-                        Cms List
-                    </Typography>
-                    <Button
-                        variant="contained"
-                        startIcon={<Icon icon={plusFill} />}
-                        onClick={() => props.openModal()}
-                    >
-                        Add Cms Page
-                    </Button>
-                </Stack>
-                <Card>
-                    <DataTableExtensions {...tableData}>
-                        <DataTable
-                            columns={columns}
-                            data={data}
-                            noHeader
-                            defaultSortField="id"
-                            defaultSortAsc={false}
-                            pagination
-                            highlightOnHover
-                        />
-                    </DataTableExtensions>
-
-                </Card>
-            </Container>
-        </Page>
-    )
-
+      <Container>
+        <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
+          <Typography variant="h4" gutterBottom>
+            Cms List
+          </Typography>
+          <Button
+            variant="contained"
+            startIcon={<Icon icon={plusFill} />}
+            onClick={() => props.openModal()}
+          >
+            Add Cms Page
+          </Button>
+        </Stack>
+        <Card>
+          <DataTableExtensions {...tableData}>
+            <DataTable
+              columns={columns}
+              data={data}
+              noHeader
+              defaultSortField="id"
+              defaultSortAsc={false}
+              pagination
+              highlightOnHover
+            />
+          </DataTableExtensions>
+        </Card>
+      </Container>
+    </Page>
+  );
 }
--- src/View/admin/Donation/organizationDonationTable.js
+++ src/View/admin/Donation/organizationDonationTable.js
@@ -1,163 +1,145 @@
 import Label from '../../../components/Label';
-import DataTable from "react-data-table-component";
-import DataTableExtensions from "react-data-table-component-extensions";
-import "react-data-table-component-extensions/dist/index.css";
+import DataTable from 'react-data-table-component';
+import DataTableExtensions from 'react-data-table-component-extensions';
+import 'react-data-table-component-extensions/dist/index.css';
 import moment from 'moment';
-import helper, { priceFormat } from '../../../Common/Helper';
+import {
+  //helper,
+  priceFormat
+} from '../../../Common/Helper';
 
 export default function OrganizationDonation(props) {
+  const columns = [
+    // { id: 'name', name: "Name", selector: "userDetails.name", sortable: true },
+    {
+      id: 'name',
+      name: 'Name',
+      cell: (row) => <>{row?.userDetails?.name ? row?.userDetails?.name : row.userId}</>,
+      ignoreRowClick: true,
+      allowOverflow: true
+    },
 
+    // { id: 'orgName', name: "Organization", selector: "organizationDetails.name", sortable: true },
 
-    const columns = [
+    {
+      id: 'orgName',
+      name: 'Organization',
+      cell: (row) => (
+        <>{row?.organizationDetails?.name ? row?.organizationDetails?.name : row.typeId}</>
+      ),
+      ignoreRowClick: true,
+      allowOverflow: true
+    },
 
-        // { id: 'name', name: "Name", selector: "userDetails.name", sortable: true },
-        {
-            id: 'name',
-            name: "Name",
-            cell: (row) => <>
+    {
+      id: 'transactionStatus',
+      name: 'Payment Status',
+      cell: (row) => (
+        <>
+          <Label
+            variant="ghost"
+            color={(row.transactionStatus === 'succeeded' && 'success') || 'error'}
+          >
+            {row.transactionStatus}
+          </Label>
+        </>
+      ),
+      ignoreRowClick: true,
+      allowOverflow: true
+    },
 
-                {row?.userDetails?.name ? row?.userDetails?.name :row.userId }
-            </>,
-            ignoreRowClick: true,
-            allowOverflow: true,
-        },
+    {
+      id: 'amount',
+      name: 'Amount',
+      cell: (row) => (
+        <>
+          <span>{(row.currencySymbol ? row.currencySymbol : '$') + priceFormat(row.amount)}</span>
+        </>
+      ),
+      ignoreRowClick: true,
+      allowOverflow: true
+    },
+    // {
+    //     name: "Date",
+    //     cell: (row) => <>
+    //         <span>{row.created_at}</span>
+    //     </>,
+    //     // ignoreRowClick: true,
+    //     sortable: true,
+    //     // allowOverflow: true,
+    // },
+    // { name: "Date", selector: "created_at", sortable: true },
+    {
+      id: 'created_at',
+      name: 'Date',
+      selector: 'created_at',
+      cell: (row) => <div>{moment(row.created_at).format('DD MMMM YYYY ')}</div>,
+      sortable: true,
+      accessor: ''
+    },
+    // {
+    //     id: 'transactionid',
+    //     name: "Transaction Id",
+    //     cell: (row) => <>
 
-        // { id: 'orgName', name: "Organization", selector: "organizationDetails.name", sortable: true },
+    //         {row.transactionId}
 
+    //     </>,
+    //     ignoreRowClick: true,
+    //     // allowOverflow: true,
+    // },
 
-        {
-            id: 'orgName',
-            name: "Organization",
-            cell: (row) => <>
+    {
+      id: 'trsansection_id',
+      name: 'Transaction Id',
+      cell: (row) => (
+        <>
+          <span>{row.uniqueTransactionId ? row.uniqueTransactionId : row.transactionId}</span>
+        </>
+      ),
+      ignoreRowClick: true,
+      allowOverflow: true
+    }
 
-                {row?.organizationDetails?.name ? row?.organizationDetails?.name :row.typeId }
-            </>,
-            ignoreRowClick: true,
-            allowOverflow: true,
-        },
+    // {
+    //     id: 'action',
+    //     name: "Actions",
+    //     cell: (row) => <>
+    //         <button className="btn btn-info btn-sm" onClick={(e) => props.viewOrderDetails(row)}>View</button>&nbsp;
+    //         {/* <button className="btn btn-sm btn-primary" onClick={() => props.editProduct(row)}><Icon icon={editfill} /></button> */}
+    //     </>,
+    //     ignoreRowClick: true,
+    //     allowOverflow: true,
+    //     button: true,
+    // },
+  ];
+  const data = [];
 
-        {
-            id: 'transactionStatus',
-            name: "Payment Status",
-            cell: (row) => <>
+  if (props.organizationDonationList && props.organizationDonationList.length > 0) {
+    props.organizationDonationList.map((item) => {
+      data.push(item);
+    });
+  }
 
-                <Label
-                    variant="ghost"
-                    color={(row.transactionStatus === "succeeded" && 'success') || 'error'}
-                >
-                    {row.transactionStatus}
-                </Label>
-            </>,
-            ignoreRowClick: true,
-            allowOverflow: true,
-        },
+  const tableData = {
+    columns,
+    data,
+    export: false,
+    print: false
+  };
 
-        {
-            id: 'amount',
-            name: "Amount",
-            cell: (row) => <>
-                <span>{(row.currencySymbol ? row.currencySymbol : "$") + priceFormat(row.amount)}</span>
-            </>,
-            ignoreRowClick: true,
-            allowOverflow: true,
-        },
-        // {
-        //     name: "Date",
-        //     cell: (row) => <>
-        //         <span>{row.created_at}</span>
-        //     </>,
-        //     // ignoreRowClick: true,
-        //     sortable: true,
-        //     // allowOverflow: true,
-        // },
-        // { name: "Date", selector: "created_at", sortable: true },
-        {
-            id: 'created_at',
-            name: 'Date',
-            selector: 'created_at',
-            cell: row => <div>{moment(row.created_at).format("DD MMMM YYYY ")}</div>,
-            sortable: true,
-            accessor: '',
-        },
-        // {
-        //     id: 'transactionid',
-        //     name: "Transaction Id",
-        //     cell: (row) => <>
-
-        //         {row.transactionId}
-
-
-        //     </>,
-        //     ignoreRowClick: true,
-        //     // allowOverflow: true,
-        // },
-
-        {
-            id: 'trsansection_id',
-            name: "Transaction Id",
-            cell: (row) => <>
-
-                <span>
-                    {row.uniqueTransactionId ? row.uniqueTransactionId : row.transactionId}
-                </span>
-            </>,
-            ignoreRowClick: true,
-            allowOverflow: true,
-        },
-
-
-        // {
-        //     id: 'action',
-        //     name: "Actions",
-        //     cell: (row) => <>
-        //         <button className="btn btn-info btn-sm" onClick={(e) => props.viewOrderDetails(row)}>View</button>&nbsp;
-        //         {/* <button className="btn btn-sm btn-primary" onClick={() => props.editProduct(row)}><Icon icon={editfill} /></button> */}
-        //     </>,
-        //     ignoreRowClick: true,
-        //     allowOverflow: true,
-        //     button: true,
-        // },
-
-
-    ];
-    const data = [];
-
-    if (props.organizationDonationList && props.organizationDonationList.length > 0) {
-
-        props.organizationDonationList.map((item) => {
-            data.push(item);
-        },
-        );
-    };
-
-
-
-
-    const tableData = {
-        columns,
-        data,
-        export: false,
-        print: false,
-    };
-
-
-
-
-
-    return (
-
-        <DataTableExtensions {...tableData}>
-            <DataTable
-                columns={columns}
-                data={data}
-                noHeader
-                defaultSortFieldId="created_at"
-                pagination
-                striped
-                highlightOnHover
-                defaultSortAsc={false}
-            />
-        </DataTableExtensions>
-    )
-
+  return (
+    <DataTableExtensions {...tableData}>
+      <DataTable
+        columns={columns}
+        data={data}
+        noHeader
+        defaultSortFieldId="created_at"
+        pagination
+        striped
+        highlightOnHover
+        defaultSortAsc={false}
+      />
+    </DataTableExtensions>
+  );
 }
