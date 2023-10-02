585e2cd2ac072f9b66895ff43c9e6a5a8e299b26
moofawsaw <kylejunderhill@gmail.com>
2023-10-01 22:27:06 -0400

    dark-mode global.scss /organisms move

--- src/View/frontEnd/Component/atoms/logo/index.js
+++ src/View/frontEnd/Component/atoms/logo/index.js
@@ -1,38 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.scss';
 import { Link } from 'react-router-dom';
-// import { setUserCountry} from "../../user/user.action"
 import { useSelector } from 'react-redux';
 import logo2 from '../../../../../assets/images/logo2.svg';
-
-// class Logo extends React.Component {
-//   render() {
-//     return (
-//       <a href="/" className="logo-wrap d-flex align-items-center">
-//         <img
-//           src=""
-//           alt="Donorport Logo Icon"
-//           className="logo-icon"
-//         />
-//         <div className="logo-name ms-1">Donorport</div>
-//       </a>
-//     );
-//   }
-// }
+import logowhite from '../../../../../assets/images/logowhite.svg';
 
 const Logo = () => {
   const user = useSelector((state) => state.user);
+  const [theme, setTheme] = useState(document.documentElement.getAttribute('data-theme'));
+
+  useEffect(() => {
+    // Create a MutationObserver to watch for changes in the data-theme attribute
+    const observer = new MutationObserver((mutationsList) => {
+      for (const mutation of mutationsList) {
+        if (mutation.attributeName === 'data-theme') {
+          setTheme(mutation.target.getAttribute('data-theme'));
+        }
+      }
+    });
+
+    const targetNode = document.documentElement;
+
+    // Configure and start the MutationObserver
+    const observerConfig = { attributes: true, attributeFilter: ['data-theme'] };
+    observer.observe(targetNode, observerConfig);
+
+    // Cleanup function
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
 
   return (
     <>
       <Link to="/" className="logo-wrap d-flex align-items-center text-decoration-none position-relative">
         <img
-          // src=""
-          src={logo2}
+          src={theme === 'dark' ? logowhite : logo2}
           alt="Donorport Logo Icon"
           className="logo-icon"
         />&nbsp;
-        {/* <span className="logo-name fs-3 ms-1 ">Donorport</span>&nbsp; */}
         <span className="logo-span">{user.countrySortName}</span>
       </Link>
     </>
